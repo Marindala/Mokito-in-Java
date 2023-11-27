ffc17d21edc8e11dc8801de56529c938d7d6f36a
Marina <95050756+Marindala@users.noreply.github.com>
2023-11-27 03:24:37 -0300

    first commit

---
+++ src/service/impl/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package service.impl;
+
+import entities.Product;
+import service.ProductService;
+import service.ShoppingCart;
+
+public class ProductServiceImpl implements ProductService {
+    private ShoppingCart cart;
+
+    public ProductServiceImpl(ShoppingCart cart) {
+        this.cart = cart;
+    }
+
+    @Override
+    public void addProductToCart(Product product, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            cart.addProduct(product);
+        }
+    }
+
+    @Override
+    public int getAllProductsFormCart() {
+        return cart.getTotalProducts();
+    }
+}
